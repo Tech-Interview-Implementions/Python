bb840698b7de8b0d6143aa475e3e747c244d8d8d
Rahul-Ravindran-Official <59519637+rahul-ravindran-official@users.noreply.github.com>
2020-01-11 20:06:12 -0500

    + Added test cases to the bubble sort implementation
    Signed-off-by: Rahul Ravindran <rahulravindran.in@gmail.com>

--- Sorting_Algorithms/Bubble_Sort/BubbleSort.py
+++ Sorting_Algorithms/Bubble_Sort/BubbleSort.py
@@ -12,3 +12,15 @@ def bubble_sort(lst: [int]) -> [int]:
                 lst[j], lst[j+1] = lst[j+1], lst[j]
 
     return lst
+
+
+def test_bubble_sort():
+    assert bubble_sort([]) == []
+    assert bubble_sort([1]) == [1]
+    assert bubble_sort([1, 2, 3, 4]) == [1, 2, 3, 4]
+    assert bubble_sort([4, 3, 2, 1]) == [1, 2, 3, 4]
+    assert bubble_sort([1, 2, 1, 2]) == [1, 1, 2, 2]
+    assert bubble_sort([1, 1, 2, 2, 2]) == [1, 1, 2, 2, 2]
+    assert bubble_sort(
+        [21, 1, 27, 30, 12, 17, 20, 28, 16, 4, 11, 5, 8, 2, 38, 26, 10, 25, 23]
+    ) == [1, 2, 4, 5, 8, 10, 11, 12, 16, 17, 20, 21, 23, 25, 26, 27, 28, 30, 38]
